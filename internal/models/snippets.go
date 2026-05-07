27c739f67bbe1a57049695e667ac6676268838a7
Erich Hafenmaier <ehafenmaier@gmail.com>
2026-05-07 11:21:39 -0500

    Complete the snippets latest function

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -65,5 +65,36 @@ func (m *SnippetModel) Get(id int) (Snippet, error) {
 
 // Latest returns the 10 most recently created snippets
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	// Create the SQL query
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+             WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	// Execute the query
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Initialize a slice for snippets
+	var snippets []Snippet
+
+	// Loop through the results and copy into the snippets slice
+	for rows.Next() {
+		var s Snippet
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	// Check for any iteration errors
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
