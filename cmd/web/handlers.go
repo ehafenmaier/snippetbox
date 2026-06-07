d4160f8678314b0202ff62c5c0cd4ee633a011ea
Erich Hafenmaier <ehafenmaier@gmail.com>
2026-06-07 16:02:58 -0700

    Add snippet creation form and update handlers for snippet creation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,15 +52,32 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	// Attempt to parse the form data
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
+	// Get the title and content from the form data
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// Get the expires number string from the form data and try to convert to integer
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Save new snippet to the database
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
