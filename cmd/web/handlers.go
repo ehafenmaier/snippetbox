d745638474f85b4e5e131c3561e366006693c46f
Erich Hafenmaier <ehafenmaier@gmail.com>
2026-08-08 08:40:38 -0700

    Move flash message retrieval to newTemplateData for improved session handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -53,16 +53,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the PopString() method to retrieve the value for the "flash" key.
-	// PopString() also deletes the key and value from the session data, so it
-	// behaves like a one-time fetch. If there is no matching key in the session
-	// data this will return the empty string.
-	flash := app.sessionManager.PopString(r.Context(), "flash")
-
 	// Create new template data struct with default data
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-	data.Flash = flash
 
 	// Use the new render helper
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
